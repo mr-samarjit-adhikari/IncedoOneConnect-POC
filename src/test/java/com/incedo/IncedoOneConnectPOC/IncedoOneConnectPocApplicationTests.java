f582129b9124ec7a2ffe08b0fe867034a2bd5f51
Samarjit Adhikari <SAdhikari@a10networks.com>
2021-11-27 13:54:00 +0530

    Incitial commit of the inceo One Connect POC Application

---
+++ src/test/java/com/incedo/IncedoOneConnectPOC/IncedoOneConnectPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incedo.IncedoOneConnectPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncedoOneConnectPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
